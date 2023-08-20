a5721767b320883536ea15fded316dc1844a1285
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-08-20 10:47:16 +0700

    first commit

---
+++ WebApi_Play_With_Docker/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi_Play_With_Docker.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
